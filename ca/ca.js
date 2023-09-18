435d2480251c21533344690ca30fbe56ccb37a4a
Heaven-Canceller <71493513+Heaven-Canceller@users.noreply.github.com>
2023-09-19 00:58:45 +0800

    Create ca.js

---
+++ ca/ca.js
@@ -0,0 +1,24 @@
+
+
+************************
+[rewrite_local]
+^https:\/\/ca\.completeanatomy\.cn\/api\/v13\/users url script-response-body ca.js
+[mimt]
+hostname = ca.completeanatomy.cn
+
+
+************************
+
+var body = $response.body;
+var url = $request.url;
+var obj = JSON.parse body);
+
+const ca = ‘/info’;
+
+if (url.indexOf(ca) != -1) {
+    obj.data.active = true;
+obj.data. expire = "2099-07-31T00:00:00Z";
+body = JSON.stringing(obj);
+}
+
+$done({body});
