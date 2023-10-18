330bc63dd6b70ede7eecfa8d027aa7890785ae70
Heaven-Canceller <71493513+Heaven-Canceller@users.noreply.github.com>
2023-10-18 17:48:08 +0800

    Update ca.js

--- ca/ca.js
+++ ca/ca.js
@@ -8,9 +8,9 @@
 *******************************
 %¥
 [rewrite_local]
-^https?:\/\/ca\.completeanatomy\.cn\/api\/v13\/users\/info.*? url script-request-header completeanatomy.js
+^https?:\/\/ca\.completeanatomy\.cn\/api\/v13\/users\/info.*? url script-request-header https://raw.githubusercontent.com/Heaven-Canceller/Qx/main/ca/ca.js
 
-^https?:\/\/ca\.completeanatomy\.cn\/api\/v13\/users\/info*? url script-response-body completeanatomy.js
+^https?:\/\/ca\.completeanatomy\.cn\/api\/v13\/users\/info*? url script-response-body https://raw.githubusercontent.com/Heaven-Canceller/Qx/main/ca/ca.js
 
 
 [mitm]
