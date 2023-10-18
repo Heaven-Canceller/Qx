300d9d8cde0f28090af9721fab856ba00a27ea9d
Heaven-Canceller <71493513+Heaven-Canceller@users.noreply.github.com>
2023-10-18 17:23:36 +0800

    更新 ca.js

--- ca/ca.js
+++ ca/ca.js
@@ -1,25 +1,48 @@
-
-什么也没有
-    
-************************
+/******************************
+⚠️如果放远程，请把completeanatomy.js替换成运程链接⚠️
+  🧚🏻‍♂️作者：🍡魔法师、木木🍡
+  wx交流群：1077223830
+🫧脚本名称:ca
+🫧建议配合working copy一起食用
+✈️working copy下载地址https://apps.apple.com/app/id896694807✈️
+*******************************
+%¥
 [rewrite_local]
-^https:\/\/ca\.completeanatomy\.cn\/api\/v13\/users url script-response-body https://raw.githubusercontent.com/Heaven-Canceller/Qx/ca/ca.js
-[mimt]
+^https?:\/\/ca\.completeanatomy\.cn\/api\/v13\/users\/info.*? url script-request-header completeanatomy.js
+
+^https?:\/\/ca\.completeanatomy\.cn\/api\/v13\/users\/info*? url script-response-body completeanatomy.js
+
+
+[mitm]
 hostname = ca.completeanatomy.cn
+%¥
+*******************************/
+
+var Url = $request.url;
+function setQueryString(key, val) { 
+    var url = Url.split('?'), search=url[1];
+    var query = {};
+    if (search) {
+        search.split('&').forEach((item) => {
+            var arr = item.split('=');
+            query[arr[0]] = arr[1];
+        });
+    }
+    query[key] = val;
+    var queryArr = [];
+    for (var p in query) {
+        queryArr.push(p + '=' + query[p]);
+    }
+    return url[0]+'?'+queryArr.join('&');
+};
 
 
-************************
+console.log(Url);
+$done({ url:Url });
 
-var body = $response.body;
-var url = $request.url;
-var obj = JSON.parse body);
-
-const ca = ‘/info’;
-
-if (url.indexOf(ca) != -1) {
-    obj.data.active = true;
-obj.data. expire = "2099-07-31T00:00:00Z";
-body = JSON.stringing(obj);
-}
-
-$done({body});
+//
+var body=$response.body;
+body = body.replace(/"active" : ([\s\S]*?)/g,'"active" : true');
+body = body.replace(/"expire" : ([\s\S]*?)/g,'"expire" : "2099-09-01T02:11:21.082126Z"');
+body = body.replace(/"active\":false,'"active":true,"');
+$done(body);
