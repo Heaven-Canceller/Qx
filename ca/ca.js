b2ff3399cf5bb24eda9cbaefbead552e76e5ad0b
Heaven-Canceller <71493513+Heaven-Canceller@users.noreply.github.com>
2023-10-27 20:12:21 +0800

    更新 completeanatomy.js

--- ca/ca.js
+++ ca/ca.js
@@ -8,39 +8,13 @@
 *******************************
 %¥
 [rewrite_local]
-^https?:\/\/ca\.completeanatomy\.cn\/api\/v13\/users\/info.*? url script-request-header https://raw.githubusercontent.com/Heaven-Canceller/Qx/main/ca/ca.js
-
-^https?:\/\/ca\.completeanatomy\.cn\/api\/v13\/users\/info*? url script-response-body https://raw.githubusercontent.com/Heaven-Canceller/Qx/main/ca/ca.js
+^https?:\/\/ca\.completeanatomy\.cn\/api\/v13\/users\/info.*? url script-response-body completeanatomy.js
 
 
 [mitm]
 hostname = ca.completeanatomy.cn
 %¥
 *******************************/
-
-var Url = $request.url;
-function setQueryString(key, val) { 
-    var url = Url.split('?'), search=url[1];
-    var query = {};
-    if (search) {
-        search.split('&').forEach((item) => {
-            var arr = item.split('=');
-            query[arr[0]] = arr[1];
-        });
-    }
-    query[key] = val;
-    var queryArr = [];
-    for (var p in query) {
-        queryArr.push(p + '=' + query[p]);
-    }
-    return url[0]+'?'+queryArr.join('&');
-};
-
-
-console.log(Url);
-$done({ url:Url });
-
-//
 var body=$response.body;
 body = body.replace(/"active" : ([\s\S]*?)/g,'"active" : true');
 body = body.replace(/"expire" : ([\s\S]*?)/g,'"expire" : "2099-09-01T02:11:21.082126Z"');
